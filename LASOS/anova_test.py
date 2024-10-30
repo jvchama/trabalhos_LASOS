cb4f7601662e24154cd560aefe43a81a8524c659
João Chanarelli <jvchamarelli@gmail.com>
2024-10-30 16:33:35 -0300

    Delete LASOS directory

--- LASOS/anova_test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-uni = pd.read_excel("BANCO_LIMPO_INT.xlsx")
